5d7b9ef63e39217a0a925a6ef8276dddc928a2e4
zhujiang <zhujiang@qq.com>
2022-03-25 13:34:31 +0800

    ss

--- workSystem/settings.py
+++ workSystem/settings.py
@@ -66,9 +66,17 @@ WSGI_APPLICATION = 'workSystem.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'worklist',
+        'USER': 'root',
+        'PASSWORD': 'Mysqldb123,./',
+        'HOST': '192.168.75.130',
+        'PORT': '3306',
     }
 }
 
